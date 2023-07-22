c5d79e6503c36ab99d164feb2127735b13d9acbc
CrisCL20 <cristian.soriano@usm.cl>
2023-07-22 01:40:26 -0400

    Changed implementation

--- src/vectors.cc
+++ src/vectors.cc
@@ -2,21 +2,13 @@
 
 namespace Vector { 
     template <class T> 
-    vector<T>::vector(T x, T y) : x(x), y(y) {}
+    void vector<T>::vector(T x, T y) : x(x), y(y) {}
 
     template<class T>
-    vector<T>::vector(T x, T y, T z) : x(x), y(y), z(z) {}
-
-    template<class T>
-    vector<T>::~vector() {}
+    void vector<T>::vector(T x, T y, T z) : x(x), y(y), z(z) {}
 
     template <class T>
-    double vector<T>::norm2d() {
-        return sqrt((x * x) + (y * y));
-    }
-
-    template <class T>
-    double vector<T>::norm3d() {
+    double vector<T>::norm() {
         return sqrt((x * x) + (y * y) + (z * z));
     }
 
@@ -32,11 +24,7 @@ namespace Vector {
 
     template <class T>
     T operator*(vector<T> a, vector<T> b) {
-        return a.x * b.x + a.y * b.y;
-    }
-
-    template<class T>
-    T operator*(vector<T> a,vector<T> b) {
         return a.x * b.x + a.y * b.y + a.z * b.z;
     }
+
 }
