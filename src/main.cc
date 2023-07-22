3b732dd91c379a7962b35b571054f64ff5fd5094
CrisCL20 <cristian.soriano@usm.cl>
2023-07-22 17:34:54 -0400

    testing vector stuff

--- src/main.cc
+++ src/main.cc
@@ -5,15 +5,24 @@
 #include "vectors.h"
 #include <iostream>
 
-
-
 using namespace std;
 using namespace Vector;
 
 int main() {
 
     vector<double> r(1.0,2.0);
-    r.print();
+    vector<double> w(2.5,1.2);
+
+    vector<double> p = r + w;
+    double k = r * w;
+
+    p.print();
+    r.unitary().print();
+
+    vector<double> x = r * 0.1;
+    x.print();
+
+    cout << k << endl;
 
     return 0;
 
