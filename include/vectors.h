bee9ccda88c5417a066fb891cd566167f0bf1966
CrisCL20 <cristian.soriano@usm.cl>
2023-07-22 02:52:47 -0400

    Implementation issues as usual

--- include/vectors.h
+++ include/vectors.h
@@ -7,16 +7,15 @@ namespace Vector {
 template <class T>
 class vector {
 public:
-    void vector(T x, T y);
-    void vector(T x, T y, T z);
+    vector(T x, T y) : x(x),y(y) {}
+    vector(T x, T y, T z) : x(x),y(y),z(z) {}
     ~vector() {}
     double norm();
+    void print();
     template<class U> friend vector<U> operator+(vector<U> a, vector<U> b);
-    template<class U> friend vector<U> operator+(vector<U> a, vector<U> b);
-    template<class U> friend U operator* ( vector<U> a, vector<U> b); //dot product
+    template<class M> friend M operator* ( vector<M> a, vector<M> b); //dot product
     
 private:
-    double ang;
     T x,y,z {0};
 };
 }
--- src/vectors.cc
+++ src/vectors.cc
@@ -1,11 +1,9 @@
 #include "vectors.h"
+#include "iostream"
+
+using namespace std;
 
 namespace Vector { 
-    template <class T> 
-    void vector<T>::vector(T x, T y) : x(x), y(y) {}
-
-    template<class T>
-    void vector<T>::vector(T x, T y, T z) : x(x), y(y), z(z) {}
 
     template <class T>
     double vector<T>::norm() {
@@ -13,17 +11,18 @@ namespace Vector {
     }
 
     template <class T>
-    vector<T> operator+(vector<T> a, vector<T> b) {
-        return new vector<T>(a.x+b.x,a.y+b.y);
+    void vector<T>::print() {
+        cout << x << "i + " << y << "j + " << z << "k\n";
     }
 
-    template <class T>
-    vector<T> operator+(vector<T> a, vector<T> b) {
-        return new vector<T>(a.x+b.x,a.y+b.y,a.z+b.z);
+    template <class U>
+    vector<U> operator+(vector<U> a, vector<U> b) {
+        return new 
+            vector<U>(a.x+b.x,a.y+b.y,a.z+b.z);
     }
 
-    template <class T>
-    T operator*(vector<T> a, vector<T> b) {
+    template <class M> 
+    M operator*(vector<M> a, vector<M> b) {
         return a.x * b.x + a.y * b.y + a.z * b.z;
     }
 
