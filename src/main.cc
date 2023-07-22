b7999bc7994c816c392bfbf75f627c4afb00d3df
CrisCL20 <cristian.soriano@usm.cl>
2023-07-22 02:52:55 -0400

    main file

---
+++ src/main.cc
@@ -0,0 +1,20 @@
+/**
+ * @brief For testing purposes
+ */
+
+#include "vectors.h"
+#include <iostream>
+
+
+
+using namespace std;
+using namespace Vector;
+
+int main() {
+
+    vector<double> r(1.0,2.0);
+    r.print();
+
+    return 0;
+
+}
