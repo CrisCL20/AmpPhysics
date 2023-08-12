4b9cf717b2375436099d654786b64a75349fccce
CrisCL20 <cristian.soriano@usm.cl>
2023-08-12 01:11:11 -0400

    added lib/ for implementation compiling

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-g++ -Wall -Wextra -Werror -Wshadow -pedantic -O2 -I/$(pwd)/include/ --std=c++20 -o engine src/*.cc 
+g++ -Wall -Wextra -Werror -Wshadow -pedantic -O2 -I/$(pwd)/include/ --std=c++20 -o engine lib/*.cpp src/main.cc  
