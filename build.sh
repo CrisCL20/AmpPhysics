9d2af6e0653fdd09fcd993876f70b110181246e4
CrisCL20 <cristian.soriano@usm.cl>
2023-08-12 12:06:51 -0400

    made a few changes for better reading

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-g++ -Wall -Wextra -Werror -Wshadow -pedantic -O2 -I/$(pwd)/include/ --std=c++20 -o engine lib/*.cpp src/main.cc  
+wFlags="-Wall -Wextra -Werror -Wshadow -pedantic"
+
+g++ $wFlags -O2 -I/$(pwd)/include/ --std=c++20 -o engine lib/*.cpp src/main.cpp -L$(pwd)/lib -lSDL2
