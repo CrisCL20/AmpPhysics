24de95f0ae1339bd4c111205bb72ceb5ec6e5a4f
CrisCL20 <cristian.soriano@usm.cl>
2023-08-17 21:03:24 -0400

    feat: implemented operator+

--- lib/matrix.cpp
+++ lib/matrix.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include "matrix.h"
 
 template <class T>
@@ -5,7 +6,6 @@ Matrix::matrix<T>::matrix(int nrows, int ncols) {
     nRows = nrows;
     nCols = ncols;
     const int N = nRows * nCols;
-    T* matdata = new T[N];
     for(size_t i = 0; i < N; ++i)
         matdata[i] = 0.0;
 }
@@ -16,13 +16,30 @@ Matrix::matrix<T>::~matrix() {
 }
 
 template <class U>
-Matrix::matrix<U>::T() {
+Matrix::matrix<U> Matrix::matrix<U>::T() {
     
     Matrix::matrix<U> B(nCols,nRows);
     for(size_t i = 0; i < nRows; ++i)
         for(size_t j = 0; j < nCols;++j)
-            B.matdata[i+j*nCols] = matdata[j+i*nRows]; 
+            B.matdata[i+j*nCols] = matdata[j+i*nRows];
+        
+    return B;
 }
 
-template <class T>
-Matrix::matrix<T> Matrix::matrix<T>::operator+(Matrix::matrix<T> other) {}
+template <class U>
+Matrix::matrix<U> Matrix::matrix<U>::operator+(Matrix::matrix<U> other) {
+    
+    int rows = Matrix::matrix<U>::getRows();
+    int cols = other.nCols;
+
+    Matrix::matrix<U> C;
+
+    C.nRows = rows;
+    C.nCols = cols;
+    
+    for(size_t i = 0; i < rows; ++i) 
+        for(size_t j = 0; j < cols; ++j)
+            C.matdata[i+j*cols] = matdata[i+j*cols] + other.matdata[i+j*cols];
+        
+    
+}
