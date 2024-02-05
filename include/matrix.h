322470853f9cbd5bb0267935ff158840423400e3
CrisCL20 <cristian.soriano@usm.cl>
2024-02-04 23:57:03 -0300

    fix: separate compilation errors and logic errors

--- include/matrix.h
+++ include/matrix.h
@@ -1,29 +1,46 @@
+#pragma once
+
 #ifndef MATRIX_H
 #define MATRIX_H
 
+#include <vector>
+#include <memory>
+
 namespace Matrix {
-    template <class U>
+    template <typename U>
     class matrix {
         public:
-            matrix(int nrows,int ncols); //creates matrix filled with zeros.
+            matrix(const std::size_t nrows,const std::size_t ncols); //creates matrix filled with zeros.
+            matrix();          
 
-            ~matrix();
+            ~matrix() {}
+            
+            /* Common matrix characteristics and properties */
+            
+            void ones(); //fills matrix A with ones 
+            void fill(const U n);
+            double determinant();
+            matrix<double> inverse();
+            matrix<U> transpose(); 
+            
+            /* Usefull operators */
+            
+            matrix<U> operator+(const matrix<U> B);
+            matrix<U> operator*(matrix<U> B); //dot product
+            matrix<U> operator&(matrix<U> B); //element-wise multiplication (essential for Hadammart product)
 
-            double det();
-            matrix<double> inv();
-            matrix<U> T(); 
-
-            matrix<U> operator+(matrix<U> other);
-            matrix<U> operator*(matrix<U> other);
-            matrix<U> operator*(const double k); 
+            U *getMatData();
+            void putData(const std::size_t i, const std::size_t j, const U dat);
 
         private:
-            int nRows;
-            int nCols;
-            U* matdata; //array with matrix data
+            std::size_t nRows;
+            std::size_t nCols;
+            U *matdata; //array with matrix data
     };
 
-    const matrix<int> identity(int dim); //generates the identity matrix on dim x dim. 
+    const matrix<bool> identity(const int dim); //generates the identity matrix on dim x dim. 
+    
+    
 }
 
 #endif
--- lib/matrix.cpp
+++ lib/matrix.cpp
@@ -1,45 +1,121 @@
-#include <cstddef>
-#include "matrix.h"
+#include <matrix.h>
 
-template <class T>
-Matrix::matrix<T>::matrix(int nrows, int ncols) {
+template <typename U>
+Matrix::matrix<U>::matrix(const std::size_t nrows, const std::size_t ncols) {
     nRows = nrows;
     nCols = ncols;
-    const int N = nRows * nCols;
-    for(size_t i = 0; i < N; ++i)
-        matdata[i] = 0.0;
+    const std::size_t N = nRows * nCols;
+    matdata = new U[N];
+    for(size_t i = 1; i <= N; ++i)
+        matdata[i] = 0;
 }
 
-template <class T>
-Matrix::matrix<T>::~matrix() {
-    delete[] matdata * (matdata!=NULL);
+template <typename U>
+Matrix::matrix<U>::matrix() {
+  matdata = 0x0;
 }
 
-template <class U>
-Matrix::matrix<U> Matrix::matrix<U>::T() {
+template <typename U>
+Matrix::matrix<U> Matrix::matrix<U>::transpose() {
     
     Matrix::matrix<U> B(nCols,nRows);
-    for(size_t i = 0; i < nRows; ++i)
-        for(size_t j = 0; j < nCols;++j)
-            B.matdata[i+j*nCols] = matdata[j+i*nRows];
+    for(std::size_t i = 1; i <= nRows; ++i)
+        for(std::size_t j = 1; j <= nCols;++j)
+            B.matdata[(i-1)*nCols+j] = matdata[(j-1)*nRows+i];
         
     return B;
 }
 
-template <class U>
-Matrix::matrix<U> Matrix::matrix<U>::operator+(Matrix::matrix<U> other) {
-    
-    int rows = Matrix::matrix<U>::getRows();
-    int cols = other.nCols;
+template <typename U>
+Matrix::matrix<U> Matrix::matrix<U>::operator+(Matrix::matrix<U> B) {
+   
+    if( (nRows != B.nRows) || (nCols != B.nCols))
+    {
+      throw "Cannot perform matrix addition\n";
+      exit(0);
+    }
 
-    Matrix::matrix<U> C;
+    Matrix::matrix<U> C(nRows,nCols);
 
-    C.nRows = rows;
-    C.nCols = cols;
-    
-    for(size_t i = 0; i < rows; ++i) 
-        for(size_t j = 0; j < cols; ++j)
-            C.matdata[i+j*cols] = matdata[i+j*cols] + other.matdata[i+j*cols];
-        
+    for(size_t i = 1; i <= nRows; ++i) 
+        for(size_t j = 1; j <= nCols; ++j)
+            C.matdata[(i-1)*nCols + j] = matdata[(i-1)*nCols + j] + B.matdata[(i-1)*nCols + j];
+    return C;     
     
 }
+
+template<typename U>
+Matrix::matrix<U> Matrix::matrix<U>::operator*(Matrix::matrix<U> B) 
+{ 
+  if(nCols != B.nRows){
+
+    throw "Can not perform matrix multiplication\n";
+    exit(0);
+  }
+
+  Matrix::matrix<U> C(nRows,B.nCols);
+  
+  for(std::size_t i = 1; i <= nRows; ++i) 
+    for(std::size_t j = 1; j <= B.nCols; ++j) 
+      for(std::size_t k = 1; k <= B.nRows; ++k)
+        C.matdata[(i-1)*B.nCols + j] += matdata[(i-1)*nCols + k] * B.matdata[(k-1)*nCols + j]; 
+
+  return C;
+}
+
+template<typename U>
+Matrix::matrix<U> Matrix::matrix<U>::operator&(Matrix::matrix<U> B)
+{
+  if(nRows != B.nRows || nCols != B.nCols)
+    throw "Can not perform element-wise multiplication\n";
+
+  Matrix::matrix<U> C(nRows,nCols);
+  for(std::size_t i = 1; i <= nRows; ++i)
+    for(std::size_t j = 1; j <= nCols; ++j)
+      C.putData(i,j,matdata[(i-1)*nRows+j] * B.getMatData()[(i-1)*nRows+j]);
+
+  return C;
+} 
+
+template<typename U>
+U *Matrix::matrix<U>::getMatData()
+{
+  return this->matdata;
+}
+
+template<typename U>
+void Matrix::matrix<U>::putData(const std::size_t i, const std::size_t j, const U dat)
+{
+  this->matdata[(i-1)*this->nRows + j] = dat;
+}
+
+const Matrix::matrix<bool> indentity(const std::size_t dim)
+{
+  Matrix::matrix<bool> I(dim,dim); 
+  for(std::size_t i = 1; i <= dim; ++i)
+    for(std::size_t j = 1; j <= dim; ++j)
+      if(i == j)
+        I.putData(i,j,1);
+  return I;
+} 
+
+template<typename U>
+void Matrix::matrix<U>::ones()
+{
+  const std::size_t n = nRows*nCols;
+  for(std::size_t i = 1; i <= n; ++i)
+    matdata[i] = 1; 
+}
+
+template<typename U>
+void Matrix::matrix<U>::fill(const U n)
+{
+  const std::size_t N = nRows*nCols;
+  for(std::size_t i = 1; i <= N; ++i)
+    matdata[i] = n; 
+}
+
+template class Matrix::matrix<int>;
+template class Matrix::matrix<float>;
+template class Matrix::matrix<double>;
+
